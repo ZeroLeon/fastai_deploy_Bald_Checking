5d39b7f766e3b44ae2c23a69e8a30fcc8c1ff828
ZeroLeon <griffith000@gmail.com>
2019-04-04 10:02:42 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=1SWLUvu9KQPFPdQj250mt-I6Q83WLuY8O?raw=1'
-export_file_name = 'export.pkl'
+export_file_name = 'hair_export.pkl'
 
 classes = ['bald', 'not_bald']
 path = Path(__file__).parent
